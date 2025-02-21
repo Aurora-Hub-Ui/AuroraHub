90dc4ac6fe75225ab1b105545df9bea7aac6237f
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-02-21 23:41:11 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 16732694052 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/Aurora-Hub/main/Loader.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/Aurora-Hub/main/Script.lua"))()
 else
     warn("This script cannot be executed in this game.")
 end
