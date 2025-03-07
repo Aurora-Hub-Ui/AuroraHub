598d024bf83941d04ce691df210d272390aba011
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-03-07 19:28:38 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/AuroraHub/refs/heads/main/Script.lua", true))()
+if table.find({13772394625, 15002061926, 16732694052}, game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/AuroraHub/refs/heads/main/Script.lua", true))()
+end
