61ce73f4dc9e14d2b89a6014fde3850ebdf30352
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-06-24 16:00:08 +0500

    Update EspLib.lua

--- EspLib.lua
+++ EspLib.lua
@@ -88,7 +88,7 @@ local function updateTag(model, defaultColor)
 	local hpLabel = tag:FindFirstChild("HPLabel")
 	local distLabel = tag:FindFirstChild("DistLabel")
 	local nameLabel = tag:FindFirstChild("NameLabel")
-	local color = (ESPConfig.rainbow and getRainbowColor() or defaultColor) or Color3.fromRGB(255, 255, 255)
+	local color = ESPConfig.rainbow and getRainbowColor() or defaultColor
 
 	if nameLabel then nameLabel.TextColor3 = color end
 	if hpLabel then
@@ -142,14 +142,14 @@ local function espAll()
 			for _, obj in pairs(itemFolder:GetChildren()) do
 				if obj.Name == "Shockwave" then
 					attachNameTag(obj, "Mass Infection")
-					updateTag(obj, Color3.fromRGB(255, 0, 0)) -- RED
+					updateTag(obj, Color3.fromRGB(255, 0, 0))
 				elseif obj.Name == "Swords" then
 					attachNameTag(obj, "Entanglement")
-					updateTag(obj, Color3.fromRGB(255, 0, 0)) -- RED
+					updateTag(obj, Color3.fromRGB(255, 0, 0))
 				elseif obj.Name == "HumanoidRootProjectile" then
 					attachNameTag(obj, "Corrupt Nature")
-					updateTag(obj, Color3.fromRGB(255, 0, 0)) -- RED
-	                        end
+					updateTag(obj, Color3.fromRGB(255, 0, 0))
+				end
 			end
 		end
 
@@ -157,10 +157,10 @@ local function espAll()
 			for _, obj in pairs(itemFolder:GetChildren()) do
 				if obj.Name == "1x1x1x1Zombie" then
 					attachNameTag(obj, "1x1x1x1 Zombie")
-					updateTag(obj, Color3.fromRGB(0, 255, 0)) -- GREEN
+					updateTag(obj, Color3.fromRGB(0, 255, 0))
 				elseif obj.Name == "PizzaDeliveryRig" then
 					attachNameTag(obj, "Pizza Delivery")
-					updateTag(obj, Color3.fromRGB(0, 255, 0)) -- GREEN
+					updateTag(obj, Color3.fromRGB(0, 255, 0))
 				end
 			end
 		end
@@ -171,7 +171,7 @@ local function espAll()
 				for _, gen in pairs(mapFolder:GetChildren()) do
 					if gen.Name == "Generator" then
 						attachNameTag(gen, "Generator")
-						updateTag(gen, Color3.fromRGB(255, 255, 0)) -- YELLOW
+						updateTag(gen, Color3.fromRGB(255, 255, 0))
 					end
 				end
 			end
