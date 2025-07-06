731428ff395ef0f3048aa9bfa6f281ded5899f11
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-07-06 16:49:29 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,133 @@
-local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+-- sneaky ass new anticheat pressure devs
 
--- config
-ESP.Players = false
-ESP.Boxes = false
-ESP.Names = true
-ESP:Toggle(true)
+if getgenv().PressureBallsLoaded then return end
+pcall(function() getgenv().PressureBallsLoaded = true end)
 
--- items
-ESP:AddObjectListener(Workspace.Server.SpawnedItems, {
-    Name = "OfudaBox2",
-    CustomName = "Box",
-    Color = Color3.fromRGB(0, 255, 255),
-    IsEnabled = "ofudaEsp"
-})
-ESP.ofudaEsp = true
+local function ClonedService(name)
+	local Service = game.GetService
+	local Reference = cloneref or function(reference) return reference end
+	return Reference(Service(game, name))
+end
+
+local ScreenGui = Instance.new("ScreenGui")
+local ttLabel = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local plrUI = ClonedService("Players").LocalPlayer:WaitForChild("PlayerGui")
+
+local function protectUI(sGui)
+	if sGui:IsA("ScreenGui") then
+		sGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
+		sGui.DisplayOrder = 999999999
+		sGui.ResetOnSpawn = false
+		sGui.IgnoreGuiInset = true
+	end
+	local cGUI = ClonedService("CoreGui")
+	local lPlr = ClonedService("Players").LocalPlayer
+
+	local function NAProtection(inst, var)
+		if inst then
+			if var then
+				inst[var] = "\0"
+			else
+				inst.Name = "\0"
+			end
+			inst.Archivable = false
+		end
+	end
+
+	if gethui then
+		NAProtection(sGui)
+		sGui.Parent = gethui()
+	elseif cGUI and cGUI:FindFirstChild("RobloxGui") then
+		NAProtection(sGui)
+		sGui.Parent = cGUI:FindFirstChild("RobloxGui")
+	elseif cGUI then
+		NAProtection(sGui)
+		sGui.Parent = cGUI
+	elseif lPlr and lPlr:FindFirstChildWhichIsA("PlayerGui") then
+		NAProtection(sGui)
+		sGui.Parent = lPlr:FindFirstChildWhichIsA("PlayerGui")
+		sGui.ResetOnSpawn = false
+	end
+end
+
+protectUI(ScreenGui)
+ttLabel.Name = "\0"
+ttLabel.Parent = ScreenGui
+ttLabel.BackgroundColor3 = Color3.fromRGB(4, 4, 4)
+ttLabel.BackgroundTransparency = 1.0
+ttLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+ttLabel.Position = UDim2.new(0.5, 0, 0.05, 0)
+ttLabel.Size = UDim2.new(0, 32, 0, 33)
+ttLabel.Font = Enum.Font.SourceSansBold
+ttLabel.Text = "God Mode (click)"
+ttLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+ttLabel.TextSize = 20.0
+ttLabel.TextWrapped = true
+ttLabel.ZIndex = 9999
+
+UICorner.CornerRadius = UDim.new(1, 0)
+UICorner.Parent = ttLabel
+
+local function draggable(frame)
+	frame.Active = true
+	frame.Draggable = true
+end
+
+local function perform()
+	local oldPivot = ClonedService("Players").LocalPlayer.Character:GetPivot()
+	local enterFunction = nil
+
+	for _, v in ipairs(ClonedService("Workspace"):GetDescendants()) do
+		if v.Name:lower() == "locker" and (v:IsA("Model") or v:IsA("BasePart")) then
+			local success, errorMsg = pcall(function()
+				for _, rem in ipairs(v:GetDescendants()) do
+					if rem.Name:lower() == "enter" and rem:IsA("RemoteFunction") then
+						enterFunction = rem
+					end
+				end
+				if enterFunction then
+					for i = 1, 5 do
+						ClonedService("Players").LocalPlayer.Character:PivotTo(v:GetPivot())
+						enterFunction:InvokeServer("true")
+						task.wait(0.1)
+					end
+				end
+			end)
+			if not success then
+				warn("Error invoking Remote: " .. errorMsg)
+			end
+			ClonedService("Players").LocalPlayer.Character:PivotTo(oldPivot)
+			if enterFunction then break end
+		end
+	end
+
+	task.wait(0.5)
+
+	local success, errorMsg = pcall(function()
+		ClonedService("Players").LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = 20
+	end)
+	if not success then warn("No humanoid") end
+
+	local eBorder = plrUI:FindFirstChild("EntityBorder", true)
+	if eBorder then
+		eBorder:GetPropertyChangedSignal("Visible"):Connect(function()
+			if eBorder.Visible then eBorder.Visible = false end
+		end)
+	end
+end
+
+local function initializeUI()
+	local txtlabel = ttLabel
+	txtlabel.Size = UDim2.new(0, 32, 0, 33)
+	txtlabel.BackgroundTransparency = 0.14
+	local textWidth = ClonedService("TextService"):GetTextSize(txtlabel.Text, txtlabel.TextSize, txtlabel.Font, Vector2.new(math.huge, math.huge)).X
+	local newSize = UDim2.new(0, textWidth + 69, 0, 33)
+	txtlabel:TweenSize(newSize, "Out", "Quint", 1, true)
+	txtlabel.MouseButton1Click:Connect(function()
+		spawn(perform)
+	end)
+	draggable(txtlabel)
+end
+
+coroutine.wrap(initializeUI)()
