e284d735ce968beba2c301e75aaed73d7a46faa4
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-07-08 23:37:20 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,11 @@
+local placeId = game.PlaceId
 
+if placeId == 12411473842 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/AuroraHub/main/Pressure.lua"))()
+elseif placeId == 99567941238278 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/AuroraHub/main/InkGame.lua"))()
+elseif placeId == 18687417158 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Aurora-Hub-Ui/AuroraHub/main/Forsaken.lua"))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Game not supported.")
+end
