06cd6648080d593e2cd4787f0caa3d8545e2b818
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-07-12 11:24:21 +0500

    Update EspLib.lua

--- EspLib.lua
+++ EspLib.lua
@@ -187,7 +187,11 @@ local function espAll()
 
 				attachNameTag(gen, "Generator")
 				updateTag(gen, Color3.fromRGB(255, 255, 0))
-			     end
+				elseif gen.Name == "FakeGenerator" then
+				attachNameTag(gen, "Fake Generator")
+			        updateTag(gen, Color3.fromRGB(170, 0, 255)) -- purple
+				end
+			    end
 		       end
 	          end
              end
