1ce934737e2cd675a40383acc55e7d8acd528de5
Aurora-Hub-Ui <yupiooipyupio@gmail.com>
2025-12-21 21:59:49 +0500

    Add files via upload

--- ss.lua
+++ ss.lua
@@ -10,7 +10,7 @@ position = root.Position
 
 local blacklist = {}
 
-local premium_users = { "Tgpeek1", "Technique12_12", "Vbn_bountyhunter", "iruzruz", "731niic", "RRQLEMONNl", "pedro377637", "blorospo", "flespos83", "prexos837", "polop7365", "Jaycol1"}
+local premium_users = { "Tgpeek1", "Technique12_12", "Vbn_bountyhunter", "iruzruz", "731niic", "RRQLEMONNl", "pedro377637", "blorospo", "flespos83", "prexos837", "polop7365", "Jaycol1", "NoSoyDekuGuys"}
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local function getTag(name)
